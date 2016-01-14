36615f4d1f6e3fd7ce52427bb387c50509d7f3dc
koziscool <kozjob@yahoo.com>
2016-01-14 16:18:03 -0600

    some pseudocode

---
+++ lib/tag_info.rb
@@ -0,0 +1,27 @@
+
+class TagInfo
+    def initialize( :options )
+        @type = options[type]
+        @id = options[id]
+        @classes = options[classes]
+        @name = options[name]
+        @text = ""
+        @children
+    end
+
+    def some_other_func
+
+    end
+
+    def other_func_2
+
+    end
+
+    def tag_output
+
+
+    end
+
+end
+
+ # = Struct.new( :type, :id, :classes, :name  )
