f1712a0a79c98849a6c19c40f32f3c7bc0ad9fdb
Kelsey James <kelseybjames@gmail.com>
2016-01-14 13:31:29 -0800

    Set up RSpec testing

---
+++ lib/parse_tag.rb
@@ -0,0 +1,27 @@
+Tag = Struct.new( :type, :id, :classes, :name  )
+
+def parse_type( tag_string )
+  type = tag_string.match(/<(.*?)[ >]/)[1]
+  type
+end
+
+def parse_classes( tag_string )
+  classes_string = tag_string.match(/class=['"](.*?)['"]/)[1].to_s
+  classes_array = classes_string.split(" ")
+  classes_array
+end
+
+def parse_id( tag_string )
+  id_string = tag_string.match(/id=['"](.*?)['"]/)[1].to_s
+  id_string
+end
+
+def parse_name( tag_string )
+  name_string = tag_string.match(/name=['"](.*?)['"]/)[1].to_s
+  name_string
+end
+
+def parse_tag( tag_string )
+  tag = Tag.new(parse_type(tag_string), parse_id(tag_string), parse_classes(tag_string), parse_name(tag_string))
+  tag
+end
---
+++ spec/parse_tag_spec.rb
@@ -0,0 +1,22 @@
+require_relative '../lib/parse_tag'
+
+describe '#parse_tag' do
+
+  let (:tag_string) { "<p class='foo bar' id='baz' name='fozzie'>" }
+  
+  it 'creates the tag type' do
+    expect(parse_tag(tag_string).type).to eq("p")
+  end
+
+  it 'creates the tag classes' do
+    expect(parse_tag(tag_string).classes).to eq(["foo", "bar"])
+  end
+
+  it 'creates the tag id' do
+    expect(parse_tag(tag_string).id).to eq("baz")
+  end
+
+  it 'creates the tag name' do
+    expect(parse_tag(tag_string).name).to eq("fozzie")
+  end
+end
